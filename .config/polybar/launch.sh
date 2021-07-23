7fb5e281b3bdf3199c75519a89ed1291b0afaf9a
Jona Löffler <j.loeffler@flybytes.de>
2021-07-23 08:34:42 +0200

    Add multi-monitor config for polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,6 +7,21 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch Polybar, using default config location ~/.config/polybar/config
-polybar bottom &
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+    if [[ $primary == $m ]]; then
+        MONITOR=$m TRAY_POS=right polybar bottom &
+    else
+        MONITOR=$m TRAY_POS=none polybar bottom &
+    fi
+  done
+fi
+
 
 echo "Polybar launched..."
